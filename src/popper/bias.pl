d31cde99b081b31b16b170b549d677f15af5f372
Nils Hoffmann <nils.hoffmann@smail.inf.h-brs.de>
2025-12-16 20:15:01 +0100

    basis set

--- src/popper/bias.pl
+++ src/popper/bias.pl
@@ -1,14 +1,19 @@
 % ---------- head ----------
-head_pred(valid_block,1).
+head_pred(valid_sudoku,1).
 
 % ---------- body predicates ----------
-body_pred(valid,1).
+body_pred(valid_piece,1).
+body_pred(get_row,3).    % get_row(+Index, +Sudoku, -Row)
+body_pred(get_col,3).    % get_col(+Index, +Sudoku, -Col)
+body_pred(get_block,3).  % get_block(+Index, +Sudoku, -Block)
 
 % ---------- types ----------
-type(valid, bool).
-type(block,(list,)).
-type(valid_block,(block,)).
+type(valid_piece,(list,)).
+type(valid_sudoku,(sudoku,)).
+type(get_row,(int,sudoku,list)).
+type(get_col,(int,sudoku,list)).
+type(get_block,(int,sudoku,list)).
 
 % ---------- allow predicate invention ----------
-max_inv_preds(2).
-max_inv_bodies(3).
+max_inv_preds(1000).
+max_inv_bodies(1000).
--- src/popper/bk.pl
+++ src/popper/bk.pl
@@ -1,11 +1,21 @@
-% ---------- block ----------
-block([_,_,_,_,_,_,_,_,_]).
-
-% ---------- BS is Valid Crutch ----------
-valid(Block) :-
-    \+ has_duplicate(Block).
+% ---------- basic validation ----------
+valid_piece(List) :-
+    \+ has_duplicate(List).
 
 has_duplicate([X|Xs]) :-
-    member(X, Xs).     
+    member(X, Xs).
 has_duplicate([_|Xs]) :-
     has_duplicate(Xs).
+
+% ---------- getters ----------
+% get_row(+Index, +Sudoku, -Row)
+get_row(N, sudoku(R1,R2,R3,R4,R5,R6,R7,R8,R9,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_), Row) :-
+    nth1(N, [R1,R2,R3,R4,R5,R6,R7,R8,R9], Row).
+
+% get_col(+Index, +Sudoku, -Col)
+get_col(N, sudoku(_,_,_,_,_,_,_,_,_,C1,C2,C3,C4,C5,C6,C7,C8,C9,_,_,_,_,_,_,_,_,_,_), Col) :-
+    nth1(N, [C1,C2,C3,C4,C5,C6,C7,C8,C9], Col).
+
+% get_block(+Index, +Sudoku, -Block)
+get_block(N, sudoku(_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,B1,B2,B3,B4,B5,B6,B7,B8,B9), Block) :-
+    nth1(N, [B1,B2,B3,B4,B5,B6,B7,B8,B9], Block).
