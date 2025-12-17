0b2df2630374faf85194f0088aa9f4ed4b6320db
Darian Rashed <darian.rashed@smail.inf.h-brs.de>
2025-12-17 17:00:46 +0100

    Sudoku Class added and printer implemented

---
+++ src/sudoku/representation.py
@@ -0,0 +1,45 @@
+class sudoku:
+    '''this class represents a sudoku.'''
+    
+    ''' This is the constructor of the class and sets the values attribute to the numbers given as a string'''
+    def __new__(self, numbers: str):
+        
+        if( len(numbers) != 81) :
+            raise ValueError(f"Expected 81 characters, but got {len(numbers)}.")
+        if( not numbers.isdigit()):
+            raise ValueError(f"A Sudoku may only contain digits as characters")
+        
+        self.values = numbers
+
+
+
+    def validator():
+        pass
+
+    def solver():
+        pass
+
+    def printer(self):
+
+        def cell(ch: str) -> str:
+            return "." if ch in ("0", ".") else ch
+
+
+        lines = []
+        border = "+-------+-------+-------+"
+        lines.append(border)
+
+        for r in range(9):
+            row = []
+            for c in range(9):
+                row.append(cell(self.values[r * 9 + c]))
+
+            lines.append(
+                "| " + " ".join(row[0:3]) +
+                " | " + " ".join(row[3:6]) +
+                " | " + " ".join(row[6:9]) + " |"
+            )
+
+            if r in (2,5,8):
+                lines.append(border)
+        return "\n".join(lines)
