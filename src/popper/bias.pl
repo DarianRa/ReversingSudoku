da34d6bb8931955a0e01cf41333b73b23b66f6bb
Nils Hoffmann <nils.hoffmann@smail.inf.h-brs.de>
2026-01-08 05:57:45 +0100

    added type check, adjust bias.pl to allow formulating rules

--- src/popper/bias.pl
+++ src/popper/bias.pl
@@ -7,27 +7,24 @@ body_pred(get_row,3).
 body_pred(get_col,3).
 body_pred(get_block,3).
 
-% ---------- types ----------
-type(row, list(int)).
-type(col, list(int)).
-type(block, list(int)). 
+type(idx, int).
 
-type(sudoku, (
-    row, row, row, row, row, row, row, row, row,
-    col, col, col, col, col, col, col, col, col,
-    block, block, block, block, block, block, block, block, block
+type(sudoku, tuple(
+    row,row,row,row,row,row,row,row,row,
+    col,col,col,col,col,col,col,col,col,
+    block,block,block,block,block,block,block,block,block
 )).
 
-% valid_piece only works on concrete row/col/block returned by getters
-type(valid_piece, (row)).
-type(valid_piece, (col)).
-type(valid_piece, (block)).
+type(valid_piece, pice).
+type(piece, row).
+type(piece, col).
+type(piece, block).
 
-type(valid_sudoku,(sudoku)).
-type(get_row,(idx,sudoku,row)).
-type(get_col,(idx,sudoku,col)).
-type(get_block,(idx,sudoku,block)).
+type(valid_sudoku, sudoku).
+type(get_row, (idx, sudoku, row)).
+type(get_col, (idx, sudoku, col)).
+type(get_block, (idx, sudoku, block)).
 
 % ---------- predicate invention ----------
-max_vars(6).
-max_body(6).
+max_vars(3).
+max_body(27).
--- src/popper/bk.pl
+++ src/popper/bk.pl
@@ -1,29 +1,41 @@
 % ---------- basic validation ----------
-valid_piece(L) :-
-    \+ has_duplicate(L).
+valid_piece(Piece) :-
+    piece_list(Piece, List),
+    \+ has_duplicate(List).
 
 has_duplicate([X|Xs]) :-
     member(X, Xs).
 has_duplicate([_|Xs]) :-
     has_duplicate(Xs).
 
+% helper to extract the list from row/col/block
+piece_list(row(List), List).
+piece_list(col(List), List).
+piece_list(block(List), List).
+
 % ---------- getters ----------
 % get_row(+Index, +Sudoku, -Row)
 get_row(N, sudoku(R1,R2,R3,R4,R5,R6,R7,R8,R9,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_), Row) :-
     idx(N),
-    nth1(N, [R1,R2,R3,R4,R5,R6,R7,R8,R9], row(Row)).  
+    nth1(N, [R1,R2,R3,R4,R5,R6,R7,R8,R9], Row).
 
 % get_col(+Index, +Sudoku, -Col)
 get_col(N, sudoku(_,_,_,_,_,_,_,_,_,C1,C2,C3,C4,C5,C6,C7,C8,C9,_,_,_,_,_,_,_,_,_), Col) :-
     idx(N),
-    nth1(N, [C1,C2,C3,C4,C5,C6,C7,C8,C9], col(Col)).
+    nth1(N, [C1,C2,C3,C4,C5,C6,C7,C8,C9], Col).
 
 % get_block(+Index, +Sudoku, -Block)
 get_block(N, sudoku(_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,B1,B2,B3,B4,B5,B6,B7,B8,B9), Block) :-
     idx(N),
-    nth1(N, [B1,B2,B3,B4,B5,B6,B7,B8,B9], block(Block)).
+    nth1(N, [B1,B2,B3,B4,B5,B6,B7,B8,B9], Block).
 
-idx(N) :-
-    integer(N),
-    N >= 1,
-    N =< 9.
+% ---------- helper ----------
+idx(1).
+idx(2).
+idx(3).
+idx(4).
+idx(5).
+idx(6).
+idx(7).
+idx(8).
+idx(9).
